19143e22a2369e6fd9737110b87d7dd44603640c
Larry Meadows <lfm@easystreet.net>
2023-07-02 21:47:21 -0700

    changes

--- bench.cpp
+++ bench.cpp
@@ -4,16 +4,14 @@ typedef sycl::queue gpuStream_t;
 #include <stdlib.h>
 #include <vector>
 #include <array>
+#include <iostream>
+#include <chrono>
 
 #define CONST_N 64
 struct spec_const_struct { double v[CONST_N]; };
 
 // specializatioh constant handle
-constexpr static sycl::specialization_id<spec_const_struct> spec_const;
-
-// specialization constant value
-#define spec_const_val kh.get_specialization_constant<spec_const>().v
-
+constexpr sycl::specialization_id<spec_const_struct> spec_const;
 
 struct indata {
   double v[CONST_N];
@@ -28,8 +26,8 @@ kernel(
   )
 {
   double s = 0;
-  for (int i = 0; i < CONST_N; ++i)
-    s += input[idx].v[i] * spec_const_val[i];
+  for (int i = 0; i < 1; ++i)
+    s += input[idx].v[i] * kh.get_specialization_constant<spec_const>().v[i];
   output[idx] = s;
 }
 
@@ -44,28 +42,41 @@ main()
 
   // initialize input
   size_t N = 1024*1024;
-  std::vector<indata> in_val(N);
+  std::vector<indata> h_in(N);
   for (int i = 0; i < N; ++i)
     for (int j = 0; j < CONST_N; ++j)
-      in_val[i].v[j] = i*j;
+      h_in[i].v[j] = 1;
+  indata *d_in = sycl::malloc_device<indata>(N, queue);
+  queue.memcpy(d_in, h_in.data(), N*sizeof(indata));
+  queue.wait();
 
-  std::vector<double> out_val(N);
 
+  std::vector<double> h_out(N);
+  double *d_out = sycl::malloc_device<double>(N, queue);
+
+  const auto start_clock = std::chrono::high_resolution_clock::now();
   auto input_bundle = sycl::get_kernel_bundle<sycl::bundle_state::input>(
     queue.get_context());
   
   input_bundle.set_specialization_constant<spec_const>(initval);
   auto exec_bundle = sycl::build(input_bundle);
+  const auto kernel_clock = std::chrono::high_resolution_clock::now();
 
   queue.submit([&](sycl::handler &cgh) {
     cgh.use_kernel_bundle(exec_bundle);
-    indata *in = in_val.data();
-    double *out = out_val.data();
     cgh.parallel_for(
-      N, [=](auto idx, sycl::kernel_handler kh) {
-	kernel(idx, kh, in, out);
+      N, [=](sycl::id<1> idx, sycl::kernel_handler kh) {
+	kernel(idx, kh, d_in, d_out);
       });
   });
 
   queue.wait();
+  const auto end_clock = std::chrono::high_resolution_clock::now();
+  queue.memcpy(h_out.data(), d_out, N*sizeof(double));
+  queue.wait();
+  std::cout << "spec const creation: " << (kernel_clock - start_clock).count()
+    << " kernel execution: " << (end_clock - kernel_clock).count() << "\n";
+  std::cout << N << " elements " << CONST_N << " spec constants " <<
+    "output[0] " << h_out[0] << " output[N/2] " << h_out[N/2] <<
+    " output[N-1] " << h_out[N-1] << "\n";
 }
