bdd5f1647ef03c5c1520cc744eb1cf06a9765347
Larry Meadows <lfm@easystreet.net>
2023-07-04 15:02:54 -0700

    simplify spec constant usage and fix iteration count and timing

--- bench.cpp
+++ bench.cpp
@@ -26,7 +26,7 @@ kernel(
   )
 {
   double s = 0;
-  for (int i = 0; i < 1; ++i)
+  for (int i = 0; i < CONST_N; ++i)
     s += input[idx].v[i] * kh.get_specialization_constant<spec_const>().v[i];
   output[idx] = s;
 }
@@ -54,28 +54,34 @@ main()
   std::vector<double> h_out(N);
   double *d_out = sycl::malloc_device<double>(N, queue);
 
+#if 0
   const auto start_clock = std::chrono::high_resolution_clock::now();
   auto input_bundle = sycl::get_kernel_bundle<sycl::bundle_state::input>(
     queue.get_context());
   
   input_bundle.set_specialization_constant<spec_const>(initval);
   auto exec_bundle = sycl::build(input_bundle);
+#endif
+
   const auto kernel_clock = std::chrono::high_resolution_clock::now();
 
-  queue.submit([&](sycl::handler &cgh) {
-    cgh.use_kernel_bundle(exec_bundle);
-    cgh.parallel_for(
-      N, [=](sycl::id<1> idx, sycl::kernel_handler kh) {
-	kernel(idx, kh, d_in, d_out);
-      });
-  });
+  for (int ntimes = 0; ntimes < 100; ++ntimes) {
+    queue.submit([&](sycl::handler &cgh) {
+      //cgh.use_kernel_bundle(exec_bundle);
+      cgh.template set_specialization_constant<spec_const>(initval);
+      cgh.parallel_for(
+	N, [=](sycl::id<1> idx, sycl::kernel_handler kh) {
+	  kernel(idx, kh, d_in, d_out);
+	});
+    });
 
-  queue.wait();
+    queue.wait();
+  }
   const auto end_clock = std::chrono::high_resolution_clock::now();
   queue.memcpy(h_out.data(), d_out, N*sizeof(double));
   queue.wait();
-  std::cout << "spec const creation: " << (kernel_clock - start_clock).count()
-    << " kernel execution: " << (end_clock - kernel_clock).count() << "\n";
+  const std::chrono::duration<double> kernel_time = end_clock - kernel_clock;
+  std::cout << " kernel execution: " << kernel_time.count() << "\n";
   std::cout << N << " elements " << CONST_N << " spec constants " <<
     "output[0] " << h_out[0] << " output[N/2] " << h_out[N/2] <<
     " output[N-1] " << h_out[N-1] << "\n";
